f789b73a56ef373663392d56de9e5e58a9a272cb
Kelvin <kstephano@gmail.com>
2022-02-18 19:09:23 +0000

    changed server listen console log

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require("./io");
 const port = process.env.PORT || 4000;
 
 server.listen(port, () =>
-  console.log(`Socket server departed from http://localhost:${port}`)
+  console.log(`Socket server departed from Heroku on port ${port}`)
 );
